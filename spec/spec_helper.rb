479ec701632361f5550983e164208e87a88b1644
Chris Patuzzo <chris@patuzzo.co.uk>
2017-10-08 12:09:43 +0100

    Set up WebMock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RACK_ENV"] ||= "test"
 require_relative "../config/application"
 
 require_all "spec/support"
+require "webmock/rspec"
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
